108dacb1d2d7d99945813d6a52ba99fa07c10da2
Alex Coulombe <alexcoulombe98@gmail.com>
2020-04-29 16:16:27 -0400

    Update README for RobotFeedback module

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MecademicRobot",
-    version="1.0.0",
+    version="1.0.1",
     author="Mecademic",
     author_email="support@mecademic.com",
-    license = "MIT",
+    license="MIT",
     description="A package to control the Mecademic Robots through python",
     long_description=long_description,
     long_description_content_type="markdown",
-    url="https://github.com/Mecademic/python_driver/tree/master",
+    url="https://github.com/Mecademic/python_driver",
     packages=setuptools.find_packages(),
+    data_files=[('',['LICENSE', 'README.md'])],
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+    ] 
 )
